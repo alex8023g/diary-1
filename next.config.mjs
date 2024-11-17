cf56f01a1e2ab60e2e3c759be47874d44b5d7544
alex8023 <alex8023@yandex.ru>
2024-11-17 18:40:31 +0300

    upd next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
   experimental: {
     serverActions: {
-      allowedOrigins: ['localhost:3000'],
+      allowedOrigins: [
+        'localhost:3000',
+        '95-163-241-119.cloudvps.regruhosting.ru',
+      ],
       // allowedForwardedHosts: ["localhost:3000"],
       // ^ You might have to use this property depending on your exact version.
     },
