754db716167f1981fccbe99144146b3c8977cc97
alex8023 <alex8023@yandex.ru>
2024-12-01 13:01:56 +0300

    add yandex oauth

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -3,11 +3,17 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 import { NextAuthOptions } from 'next-auth';
 import { prisma } from '../../../../../prisma/prismaClient/prismaClient';
 import GoogleProvider from 'next-auth/providers/google';
+import YandexProvider from 'next-auth/providers/yandex';
 
 if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET)
   throw new Error('GITHUB_ID and GITHUB_SECRET must be set in .env');
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET)
+  throw new Error(
+    'GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET must be set in .env',
+  );
+
+if (!process.env.YANDEX_CLIENT_ID || !process.env.YANDEX_CLIENT_SECRET)
   throw new Error(
     'GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET must be set in .env',
   );
@@ -22,6 +28,10 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    YandexProvider({
+      clientId: process.env.YANDEX_CLIENT_ID,
+      clientSecret: process.env.YANDEX_CLIENT_SECRET,
     }),
     /* 
     CredentialsProvider({
